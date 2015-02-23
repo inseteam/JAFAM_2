5ac7138becad72082f1646e0faeea54ee3722d3e
Marcus Cole <up647395@myport.ac.uk>
2015-02-23 21:37:24 +0000

    Small tweak to fix text flow

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Welcome</title>
+  <link href="styles.css" rel="stylesheet" type="text/css" media="only screen and (min-width:480px)">
+
+  <script type="text/javascript" src='lib/ajax.js'> </script>
+  <script type="text/javascript" src='lib/buttons.js'> </script>
+  <script type="text/javascript" src='lib/menu.js'> </script>
+
+</head>
+<body>
+  <header>
+    
+  </header>
+  <nav id="navigation">
+    <input type="button" value="Home" id="home"></input>
+    <input type="button" value="Create!" id="createButton"></input>
+    <input type="button" value="About Us" id="about_us"></input>
+    <input type="button" value="Help" id="about_charts"></input>
+  </nav>
+  <article>
+      <section class="Notice">
+          <h1>Welcome</h1>
+          <p>This website will allow you to create Work Breakdown Trees PERT Charts
+              and Gantt charts quickly and easily. To create a chart, click on the create tab.
+          </p>
+      </section>
+    <section class="mainPage">
+      <h1>
+        Charts Made Easy.
+      </h1>
+      <section id="content"></section>
+      <iframe name="table_content" id="table_content"></iframe>
+    </section>
+  </article>
+</body>
+</html>
