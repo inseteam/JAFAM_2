f3d0e5e27bedd134e58a8cfcca893270a4f22dc8
alexpiggott <piggott.alex@yahoo.co.uk>
2015-03-04 10:41:21 +0000

    Content change test

--- api/index.php
+++ api/index.php
@@ -1,8 +1,6 @@
-<?php
-if (!file_exists("content/index.txt"))
-echo "---- No content yet ----";
-else
-{ $filehandle = fopen("content/index.txt", "r");
-  while (!feof($filehandle))
-  echo fgets($filehandle); }
-  ?>
+<!--This is the content that will be displayed on the Home page-->
+
+<p>Ever wanted to create a Gantt chart, PERT chart, or Work Breakdown Tree, then realised that any software
+that does this is ridiculously over-complicated? Well, we're here to help. To see just how easy it can be
+to create professional looking charts, click the 'Create' tab. Or, to find out more about each individual
+chart, click the 'Help' tab.</p>
