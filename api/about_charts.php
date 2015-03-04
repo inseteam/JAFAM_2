ff97c4a081d1037bf44d820432262617bd4df587
alexpiggott <piggott.alex@yahoo.co.uk>
2015-03-04 11:01:56 +0000

    Updated Help page

--- api/about_charts.php
+++ api/about_charts.php
@@ -1,8 +1,14 @@
-<?php
-if (!file_exists("content/about_charts.txt"))
-echo "---- No content yet ----";
-else
-{ $filehandle = fopen("content/about_charts.txt", "r");
-  while (!feof($filehandle))
-  echo fgets($filehandle); }
-  ?>
+<!--This is the content that will be displayed on the Help page-->
+<!--Needs to be updated to show how to use the Create form (once it's complete)-->
+
+<h1>Gantt Charts</h1>
+<p>A Gantt chart is a graphical representation of a project's schedule. To create one of these, you'll
+need to enter the following details:</p>
+<ul>
+    <li><strong>Task ID:</strong> A unique number to identify a task.</li>
+    <li><strong>Task Name:</strong> A brief description of the task.</li>
+    <li><strong>Predecessors:</strong> The IDs of the tasks that <em>must</em> precede this task.</li>
+    <li><strong>Duration:</strong> Length of the task (days, weeks, or months).</li>
+    <li><strong>Start Date:</strong> Date that the task will begin.</li>
+    <li><strong>End Date:</strong> Date that the task will end.</li>
+</ul>
